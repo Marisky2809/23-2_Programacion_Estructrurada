5a1e46d0c9d237e6b020126418ea1c212210db53
Toysabroso <danymariscalg23@gmail.com>
2023-04-19 07:34:05 -0600

    2023_04_17_006_Bucles_V2
    Es la versión actualizada del programa de bucles. Ya puede calcular el número de enfrentamientos que se deben hacer en 3 formatos de competición diferentes.

--- 2023_04_17_006_Bucles_V1/2023_04_17_006_Bucles_V1.cpp
+++ 2023_04_17_006_Bucles_V1/2023_04_17_006_Bucles_V1.cpp
@@ -12,12 +12,19 @@
 
 int main()
 {
-    int peleadores;
-    int formato;
+    int peleadores = 0;
+    int formato = 0;
     int royal = 0;
     int directa = 0;
     int grupos = 0;
     int a = 0;
+    int d1 = 0;
+    int grupo = 0;
+    int cruces = 0;
+    int mundialito = 0;
+    int f_grupos = 0;
+    int sobrantes = 0;
+    int extras = 0;
 
     setlocale(LC_ALL, "es_MX.UTF-8");
     std::cout << "Hola, te voy a ayudar a contar cuántas batallas se necesitan para obtener a un ganador\n" << std::endl;
@@ -25,27 +32,66 @@ int main()
     std::cin >> peleadores;
     std::cout << "Qué tipo de batalla habrá?\n1) Royal Rumble\n2) Eliminatoria directa\n3) Grupos, semifinal y final" << std::endl;
     std::cin >> formato;
+
     switch (formato) {
+
+
     case 1:
-        
+
         for (int f = 1; f < peleadores; f++) {
             royal = (peleadores - f) + royal;
         }
         std::cout << "Entonces será Royal Rumble?\nExcelente :D\n" << std::endl;
-        std::cout << "Entonces el número de batallas será de " << royal <<  ". El mejor de la batalla será el ganador." << std::endl;
+        std::cout << "Entonces el número de batallas será de " << royal << ". Cada peleador se enfrentará a los demás." << std::endl;
         break;
+
+
     case 2:
+        std::cout << "Entonces será por eliminación directa?\nCool :)\n" << std::endl;
         for (int n = 1; n < peleadores; ) {
             n = n * 2;
             a = n;
         }
-        directa = a - peleadores;
-        std::cout << "Entonces será por eliminación directa?\nCool :)\n" << std::endl;
-        std::cout << "Entonces el número de batallas será de " << directa << " batallas." << std::endl;
+        if (a == peleadores) {
+            directa = (a - 1);
+            std::cout << "Entonces el número de batallas será de " << directa << " batallas." << std::endl;
+        }
+        else {
+            d1 = (a / 2);
+            directa = (peleadores - 1);
+            std::cout << "Entonces el número de batallas será de " << directa << " batallas. Aunque recomendaría hacerla de " << d1 << " peleadores." << std::endl;
+        }
         break;
+
+
     case 3:
-        std::cout << "Entonces será por grupos, semifinal y final?\nOk ok, ese es complicado de calcular, pero me gusta mucho...\n" << std::endl;
-        std::cout << "Entonces el número de batallas será de ..." << std::endl;
+        if (peleadores < 4) {
+            std::cout << "Lo siento, para calcular una competición con este formato debe haber por lo menos 4 participantes :(";
+        }
+        else {
+            cruces = (peleadores / 4);
+            for (int g = 1; g < cruces; g++) {
+                grupo = (cruces - g) + grupo;
+            }
+            f_grupos = grupo * 4;
+            sobrantes = peleadores % 4;
+            extras = sobrantes * cruces;
+            mundialito = f_grupos + extras + 3;
+
+            std::cout << "Entonces será por grupos, semifinal y final?\nOk ok, ese es complicado de calcular, pero me gusta mucho...\n" << std::endl;
+            std::cout << "Entonces el número de batallas será de " << mundialito << " enfrentamientos." << std::endl;
+            if (sobrantes == 0) {
+                std::cout << "Los grupos serían de " << cruces << " peleadores. Los mejores de cada grupo pasan a las semifinales :)";
+            }
+            else {
+                std::cout << "Los grupos serían de " << cruces << " peleadores. Aunque habría " << sobrantes << " grupos con un peleador más.";
+            }
+        }
+
+
+        break;
+
+
     default:
         std::cout << "Híjole, joven. Esa no se la vengo manejando." << std::endl;
     }
